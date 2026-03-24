4e419f2fe62dd79b97c543d98436c28c2a8fb6bd
hwan001 <woghks7209@gmail.com>
2026-03-24 18:22:46 +0900

    refactor(terraform): reorganize directory structure (infrastructure, provisions, sandbox)

---
+++ bootstrap/terraform/provisions/proxmox-vm/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "Telmate/proxmox"
+      version = "3.0.1-rc6"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_api_url          = var.proxmox_api_url
+  pm_api_token_id     = var.proxmox_api_token_id
+  pm_api_token_secret = var.proxmox_api_token_secret
+  pm_tls_insecure     = var.proxmox_tls_insecure
+}
+
+module "vm" {
+  source = "git::https://github.com/h001-lab/OpenCSP-modules.git//terraform/proxmox/vm?ref=main"
+
+  vm_name                  = var.vm_name
+  vm_id                    = var.vm_id
+  cores                    = var.cores
+  memory                   = var.memory
+  disk_size                = var.disk_size
+  vm_ip                    = var.vm_ip
+  vm_gw                    = var.vm_gw
+  vm_network_bridge        = var.vm_network_bridge
+  target_node              = var.target_node
+  template_name            = var.template_name
+  storage_pool             = var.storage_pool
+  snippet_storage_pool     = var.snippet_storage_pool
+  vm_ssh_public_key        = var.vm_ssh_public_key
+  vm_password              = var.vm_password
+  pve_host_address         = var.pve_host_address
+  pve_ssh_user             = var.pve_ssh_user
+  pve_ssh_private_key_path = var.pve_ssh_private_key_path
+}
---
+++ bootstrap/terraform/provisions/proxmox-vm/variables.tf
@@ -0,0 +1,29 @@
+# Provider
+variable "proxmox_api_url" {}
+variable "proxmox_api_token_id" { sensitive = true }
+variable "proxmox_api_token_secret" { sensitive = true }
+variable "proxmox_tls_insecure" { default = true }
+
+# API
+variable "vm_name" {}
+variable "vm_id" { type = number }
+variable "cores" { default = 1 }
+variable "memory" { default = 2048 }
+variable "disk_size" { default = "50G" }
+variable "vm_ip" { default = null }
+variable "vm_gw" { default = null }
+variable "vm_network_bridge" { default = "vmbr0" }
+variable "target_node" { default = "pve" }
+variable "template_name" { default = "ubuntu-2404-template" }
+variable "storage_pool" { default = "local-lvm" }
+variable "snippet_storage_pool" { default = "local" }
+
+# K3S Secret
+variable "vm_ssh_public_key" {}
+variable "vm_password" {
+  sensitive = true
+  default   = null
+}
+variable "pve_host_address" {}
+variable "pve_ssh_user" { default = "root" }
+variable "pve_ssh_private_key_path" {}
